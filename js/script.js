71ec39995d4850b8a78d7d9883789e5a58c71627
SoulaimanB <105544966+SoulaimanB@users.noreply.github.com>
2022-07-07 01:29:05 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .nav');
+let header = document.querySelector('.header');
+
+menu.onclick = () =>{
+   menu.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+   menu.classList.remove('fa-times');
+   navbar.classList.remove('active');
+
+  if(window.scrollY > 0){
+     header.classList.add('active');
+  }else{
+     header.classList.remove('active');
+  }
+
+}
+
+
+
+
+
+
+
+
+$(function () {
+   $('#myDiv').floatingWhatsApp({
+     phone: '+212 605-565353',
+     position:'right'
+   });
+ });
+
+
+
+//Reviews
